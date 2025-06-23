129365c4c5262676834257e2a14ff594234e47fe
Ras7id6 <147885117+Ras7id6@users.noreply.github.com>
2025-06-23 19:01:25 +0300

    commit3

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ const savedTheme = localStorage.getItem(STORAGE_KEYS.THEME);
         if (savedFilter) {
           currentFilter = savedFilter;
         }
+    } catch (error) {
+        console.error('Error loading from localStorage:', error);
+      }
+    }
