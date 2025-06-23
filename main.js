2fb8884edad9b6eefd370cb836b96fc42062492b
Ras7id6 <147885117+Ras7id6@users.noreply.github.com>
2025-06-23 19:00:23 +0300

    commit2

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const savedTheme = localStorage.getItem(STORAGE_KEYS.THEME);
         if (savedTheme) {
           currentTheme = savedTheme;
         }
+        const savedFilter = localStorage.getItem(STORAGE_KEYS.FILTER);
+        if (savedFilter) {
+          currentFilter = savedFilter;
+        }
